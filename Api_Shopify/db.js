ca8cbf102c71d4dad1fa67bc04dcc6a1a49c0c96
Tolus92 <125876824+Tolus92@users.noreply.github.com>
2023-04-18 12:17:02 +0200

    Update db.js

--- Api_Shopify/db.js
+++ Api_Shopify/db.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://TolusNowa:Nowamarket123456789@clustertest.nzjt155.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connecté'))
     .catch(err => console.log(err));
 
